a538cddf3faf1ec760dd7bc413f42282186bfae8
123456789zhang <1245985993@qq.com>
2019-09-25 17:31:55 +0800

    Car

--- CarTestLoadingScreen/Private/VehicleGameLoadingScreen.cpp
+++ CarTestLoadingScreen/Private/VehicleGameLoadingScreen.cpp
@@ -3,6 +3,75 @@
 #include "VehicleGameLoadingScreen.h"
 #include "MoviePlayer.h"
 #include "Modules/ModuleManager.h"
+#include "SlateBasics.h"
+#include "SlateExtras.h"
+
+struct FVehicleGameLoadingScreenBrush : public FSlateDynamicImageBrush, public FGCObject
+{
+	FVehicleGameLoadingScreenBrush(const FName InTextureName, const FVector2D& InImageSize)
+		: FSlateDynamicImageBrush(InTextureName, InImageSize)
+	{
+		SetResourceObject(LoadObject<UObject>(nullptr, *InTextureName.ToString()));
+	}
+
+	virtual void AddReferencedObjects(FReferenceCollector& Collector)
+	{
+		FSlateBrush::AddReferencedObjects(Collector);
+	}
+
+};
+
+
+class SVehicleLoadingScreen : public SCompoundWidget
+{
+public:
+
+	SLATE_BEGIN_ARGS(SVehicleLoadingScreen){}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs)
+	{
+		static const FName LoadingScreenName(TEXT("/Game/UI/Menu/LoadingScreen.LoadingScreen"));
+		LoadingScreenBrush = MakeShareable(new FVehicleGameLoadingScreenBrush(LoadingScreenName,FVector2D(1920,1080)));
+	
+		ChildSlot
+			[
+				SNew(SOverlay)
+				+SOverlay::Slot()
+				.HAlign(HAlign_Center)
+				.VAlign(VAlign_Fill)
+				[
+					SNew(SImage)
+					.Image(LoadingScreenBrush.Get())
+				]
+				+SOverlay::Slot()
+				.HAlign(HAlign_Fill)
+				.VAlign(VAlign_Fill)
+				[
+					SNew(SVerticalBox)
+					+SVerticalBox::Slot()
+					.VAlign(VAlign_Bottom)
+					.HAlign(HAlign_Right)
+					.Padding(FMargin(10.0f))
+					[
+						SNew(SThrobber)
+						.Visibility(this,&SVehicleLoadingScreen::GetLoadIndicatorVisibility)
+					]
+				]
+			];
+	}
+
+private:
+
+	EVisibility GetLoadIndicatorVisibility() const
+	{
+		return GetMoviePlayer()->IsLoadingFinished() ? EVisibility::Collapsed : EVisibility::Visible;
+	}
+
+	/** ¼ÓÔØÆÁÄ»µÄÍ¼Æ¬»­Ë¢ */
+	TSharedPtr< FVehicleGameLoadingScreenBrush> LoadingScreenBrush;
+};
+
 
 class FVehicleGameLoadingScreenModule : public IVehicleGameLoadingScreenModule
 {
